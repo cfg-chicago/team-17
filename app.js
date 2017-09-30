64601ba4bcd66cece0d749a68cc491dec681d923
Tyler Spagnolo <tylerspagnolo@Tylers-MacBook-Pro.local>
2017-09-29 21:12:58 -0500

    Finished architecture and views directory

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000
 var app = express();
 
-
+app.set('view engine', 'ejs');
 
 
 app.get('/', function(req,res){
-	res.render("home.html");
+	res.render("home");
 });
 
 app.get('/groups', function(req, res){
-	res.send("groups.html");
+	res.render("groups");
 });
 
-app.get('/groups/timeline', function(){})
-
+app.get('/timeline', function(req, res){
+	res.render("timeline");
+});
 
 app.listen(port, function(){
 	console.log('Server is running on port 3000');

24700a200c658e2076c214f438edcb1baa8594f0
Tyler Spagnolo <tspags97@gmail.com>
2017-09-30 05:22:36 -0500

    App.js finally got a little db goin

--- app.js
+++ app.js
@@ -5,9 +5,39 @@ var app = express();
 
 //var fourthGrade = {'grade':'4', 'students':{'number':'10', 'names':['DG', 'Dan', 'Alyssa']}};
 
-var data1 = {'groups':['Group 1', 'Group 2', 'Group 3']};
-var data2 = {'students':['Dan', 'Tyler', 'Alyssa']};
+//var data1 = {'groups':['Group 1', 'Group 2', 'Group 3']};
+//var data2 = {'students':['Dan', 'Tyler', 'Alyssa']};
 
+
+//Database
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/wff");
+
+//Schema Setup
+var groupSchema = new mongoose.Schema({
+	grade: Number,
+	names: Array,
+	teams: Array
+});
+
+
+//Compile into a model
+var Group = mongoose.model("Group", groupSchema);
+
+Group.create(
+	{
+		grade: 4,
+		students: [{name:'Tyler', team:0}, {name:'Dan', team:0}, {name:'Michael', team:0}, {name:'Bill', team:1}, {name:'Lamont', team:1}, {name:'Tisa', team:2}, {name:'Johnny', team:2} ],
+		teamNames: ['Cheetah Girls', 'Mario Brothers', 'Fudge Sticks']
+	}, function(err, group){
+		if(err){
+			console.log('SOMETHING WENT WRONG');
+		} else {
+			console.log('NEWLY CREATED GROUP:');
+			console.log(group);
+			console.log()
+		}
+	});
 
 
 app.set('view engine', 'ejs');
@@ -31,8 +61,24 @@ app.get('/home', function(req,res){
 app.get('/groups', function(req, res){
 	console.log(req.query);
 	if(req.query.grade == 4){
-		res.render('groups', {data1:data1, data2:data2});
-		console.log('made it');
+
+		//Store Database data in a variable
+		Group.find({}, function(err, fourth){
+		if(err){
+			console.log(err);
+		} else {
+
+			res.render('groups', {fourth:fourth});
+			console.log('made it');
+		}
+
+
+		
+		});
+
+
+
+		
 	} else{
 		res.render("groups");
 		console.log('Didnt made it');

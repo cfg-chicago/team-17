39e69b33e0f4ca1055d35d07277e60fbf95f3d68
Tyler Spagnolo <tylerspagnolo@Tylers-MacBook-Pro.local>
2017-09-29 22:00:55 -0500

    Add additional routes

--- app.js
+++ app.js
@@ -6,18 +6,34 @@ var app = express();
 app.set('view engine', 'ejs');
 
 
+
+//Login Page
 app.get('/', function(req,res){
+	
+	//Change to login if implemented
 	res.render("home");
 });
 
+
+//Home Page (Where all Grades will be displayed)
+app.get('/home', function(req,res){
+	res.render("home");
+});
+
+
+//Group Page (Where groups within grades will be displayed)
 app.get('/groups', function(req, res){
 	res.render("groups");
 });
 
+
+//Profile Page for each 
 app.get('/timeline', function(req, res){
 	res.render("timeline");
 });
 
+
+//Where to listen
 app.listen(port, function(){
 	console.log('Server is running on port 3000');
 });

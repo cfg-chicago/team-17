e5377b997f09f8569b769e3fda20d21553f9324d
Daniel Grichevsky <dgrichevsky@gmail.com>
2017-09-30 06:43:23 -0500

    mongodb

--- app.js
+++ app.js
@@ -2,22 +2,40 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000
 var app = express();
-
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.json());
+var db;
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
 
+// Connection URL
+var url = 'mongodb://localhost:27017';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+	db = db;
+ // db.close();
+});
 
 //Login Page
 app.get('/', function(req,res){
-	
+
 	//Change to login if implemented
 	res.render("home");
 });
 
 
 //Home Page (Where all Grades will be displayed)
-app.get('/home', function(req,res){
-	res.render("home");
+app.get('/home', function(req,res) {
+	db.collection('classmates', function(err, collection) {
+		console.log("found collection");
+	    collection.find().toArray(function(err, items) {
+		console.log(items);
+		res.send(items);
+	    });
+	});
 });
 
 
@@ -27,13 +45,15 @@ app.get('/groups', function(req, res){
 });
 
 
-//Profile Page for each 
+//Profile Page for each
 app.get('/timeline', function(req, res){
+
 	res.render("timeline");
 });
 
 
-//Where to listen
+
+
 app.listen(port, function(){
 	console.log('Server is running on port 3000');
 });

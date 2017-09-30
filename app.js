00c4773f7fb9db2c30194d62cfc645a8a29f7b88
Tyler Spagnolo <tspags97@gmail.com>
2017-09-30 05:53:12 -0500

    Added db copy

--- app.js
+++ app.js
@@ -3,13 +3,8 @@ var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
 var app = express();
 
-//var fourthGrade = {'grade':'4', 'students':{'number':'10', 'names':['DG', 'Dan', 'Alyssa']}};
 
-//var data1 = {'groups':['Group 1', 'Group 2', 'Group 3']};
-//var data2 = {'students':['Dan', 'Tyler', 'Alyssa']};
-
-
-//Database
+//Database Setup
 var mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost/wff");
 
@@ -24,20 +19,109 @@ var groupSchema = new mongoose.Schema({
 //Compile into a model
 var Group = mongoose.model("Group", groupSchema);
 
+
+//CREATE DATABASE
+/**********************************************************************************/
 Group.create(
 	{
-		grade: 4,
-		students: [{name:'Tyler', team:0}, {name:'Dan', team:0}, {name:'Michael', team:0}, {name:'Bill', team:1}, {name:'Lamont', team:1}, {name:'Tisa', team:2}, {name:'Johnny', team:2} ],
-		teamNames: ['Cheetah Girls', 'Mario Brothers', 'Fudge Sticks']
+		fourth: 
+			{
+			grade: 4,
+			students: [
+						{name:'Tyler', team:0}, 
+						{name:'Dan', team:0}, 
+						{name:'Michael', team:0}, 
+						{name:'Bill', team:1}, 
+						{name:'Lamont', team:1}, 
+						{name:'Tisa', team:2}, 
+						{name:'Johnny', team:2} 
+					],
+			teamNames: ['Cheetah Girls', 'Mario Brothers', 'Fudge Sticks']
+			},
+
+		fifth: 
+			{
+			grade: 5,
+			students: [
+						{name:'Emma', team:0}, 
+						{name:'Noah', team:0}, 
+						{name:'Liam', team:0}, 
+						{name:'Olivia', team:1}, 
+						{name:'Ava', team:1}, 
+						{name:'Ethan', team:2}, 
+						{name:'Mason', team:2} 
+					],
+			teamNames: ['Warriors', 'Team Morgan', 'Rough Riders']
+			},
+
+		sixth:
+			{
+			grade: 6,
+			students: [
+						{name:'Mia', team:0}, 
+						{name:'Oliver', team:0}, 
+						{name:'Aiden', team:0}, 
+						{name:'Charlotte', team:1}, 
+						{name:'Amelia', team:1}, 
+						{name:'Harper', team:2}, 
+						{name:'James', team:2} 
+						],
+			teamNames: [
+						'Cowboys', 
+						'Princesses', 
+						'Team Sixth Graders'
+						]
+			},
+
+		seventh:
+			{
+			grade: 7,
+			students: [
+						{name:'Ben', team:0}, 
+						{name:'Emily', team:0}, 
+						{name:'Abigail', team:0}, 
+						{name:'Logan', team:1}, 
+						{name:'Avery', team:1}, 
+						{name:'Sofia', team:2}, 
+						{name:'Ella', team:2} 
+						],
+			teamNames: [
+						'G.I. Joe', 
+						'Donkey Kong', 
+						'Owls'
+						]
+			},
+
+		eigth:
+			{
+			grade: 8,
+			students: [
+						{name:'Aria', team:0}, 
+						{name:'Evelyn', team:0}, 
+						{name:'Chloe', team:0}, 
+						{name:'Scarlet', team:1}, 
+						{name:'William', team:1}, 
+						{name:'Gabriel', team:2}, 
+						{name:'Lily', team:2} 
+						],
+			teamNames: ['The Best Group', 'Banking Fans','Strawberry Girls']
+			}
+
+
+
 	}, function(err, group){
 		if(err){
 			console.log('SOMETHING WENT WRONG');
 		} else {
 			console.log('NEWLY CREATED GROUP:');
 			console.log(group);
-			console.log()
+			console.log();
 		}
 	});
+
+/**********************************************************************************/
+
+
 
 
 app.set('view engine', 'ejs');
@@ -63,12 +147,12 @@ app.get('/groups', function(req, res){
 	if(req.query.grade == 4){
 
 		//Store Database data in a variable
-		Group.find({}, function(err, fourth){
+		Group.find({}, function(err, groups){
 		if(err){
 			console.log(err);
 		} else {
 
-			res.render('groups', {fourth:fourth});
+			res.render('groups', {groups:groups});
 			console.log('made it');
 		}
 
